acdd3aa79c0e26ab62fd4a34bad7dc99b356a5c8
heymanlean <heymanlean@gmail.com>
2019-05-27 23:09:27 +0800

    a

--- lock.py
+++ lock.py
@@ -19,9 +19,9 @@ def acquire_lock(lockname, acquire_timeout=10):
     end = time.time() + acquire_timeout
 
     while time.time() < end:
-        if db.setnx('lock:' + lockname, identifier)
+        if db.setnx('lock:' + lockname, identifier):
             return identifier
-        
+
         time.sleep(.001)
     return False
 
