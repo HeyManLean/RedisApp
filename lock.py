9303a24578bfa7a11f454a5210d1df2fbe27d10e
heymanlean <heymanlean@gmail.com>
2019-05-28 08:08:23 +0800

    信号量锁

--- lock.py
+++ lock.py
@@ -6,6 +6,7 @@ redis 锁
 """
 import uuid
 import time
+import math
 
 from redis import Redis
 from redis.exceptions import WatchError
@@ -21,6 +22,23 @@ def acquire_lock(lockname, acquire_timeout=10):
     while time.time() < end:
         if db.setnx('lock:' + lockname, identifier):
             return identifier
+
+        time.sleep(.001)
+    return False
+
+
+def acquire_lock_with_timeout(lockname, acquire_timeout=10, lock_timeout=10):
+    identifier = str(uuid.uuid4())
+    lock_timeout = int(math.ceil(lock_timeout))
+    lockname = 'lock:' + lockname
+
+    end = time.time() + acquire_timeout
+    while time.time() < end:
+        if db.setnx(lockname, identifier):
+            db.expire(lockname, lock_timeout)
+            return identifier
+        elif db.ttl(lockname) <= 0:
+            db.expire(lockname, lock_timeout)
 
         time.sleep(.001)
     return False
@@ -50,9 +68,61 @@ def purchase_item_with_lock(itemid):
     locked = acquire_lock(lockname)
     if not locked:
         return False
-    
-    pipeline = db.pipeline()
+
     try:
         pass
     finally:
         release_lock(lockname, locked)
+
+
+# ------- 信号量 -------
+def acquire_semaphore(semname, limit, timeout=10):
+    identifier = str(uuid.uuid4())
+    now = time.time()
+
+    pipeline = db.pipeline()
+    pipeline.zremrangebyscore(semname, '-inf', now - timeout)
+    pipeline.zadd(semname, identifier, now)
+    pipeline.zrank(semname, identifier)
+    if pipeline.execute()[-1] < limit:
+        return identifier
+
+    db.zrem(semname, identifier)
+    return None
+
+
+def acquire_faire_semaphore(semname, limit, timeout=10):
+    identifier = str(uuid.uuid4())
+    now = time.time()
+    czset = semname + ':owner'
+    ctr = semname + ':counter'
+
+    pipeline = db.pipeline()
+    pipeline.zremrangebyscore(semname, '-inf', now - timeout)
+    pipeline.zinterstore(czset, {czset: 1, semname: 0})
+
+    pipeline.incr(ctr)
+
+    counter = pipeline.execute()[-1]
+
+    pipeline.zadd(semname, identifier, now)
+    pipeline.zadd(czset, identifier, counter)
+
+    pipeline.zrank(czset, identifier)
+    if pipeline.execute()[-1] < limit:
+        return identifier
+
+    pipeline.zrem(semname, identifier)
+    pipeline.zrem(czset, identifier)
+    pipeline.execute()
+    return None
+
+
+def release_semaphore(semname, identifier):
+    db.zrem(semname, identifier)
+
+
+if __name__ == '__main__':
+    db.expire('aaaaaa', 3)
+    time.sleep(3)
+    print(db.ttl('aaaaaa'))
